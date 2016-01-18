9a142be1a3a6fb71e3a934fe3efe62cfbb66ac2e
cheryl.cai <cailuruoxin@gmail.com>
2016-01-18 15:23:13 +0800

    Update download_image.py
    remove reluctant code

--- download_image.py
+++ download_image.py
@@ -13,35 +13,35 @@ def Based_on_user_choice(choice):
     return
 
 def Download_latest_one():
+    total_number = Show_total_number()
+    format_total_number= int(total_number)
     image_url="http://www.gunnerkrigg.com/comics/" + total_number + ".jpg";
-    image = urllib2.urlopen(image_url)
-    output = open(format_total_number + ".jpg", "wb")
-    output.write(image.read())
-    output.close()
-    print "The latest comic image is saved as " + total_number + ".jpg in your local device"
+    Download_image(image_url, total_number)
+    print "The latest comic image is saved as " + str(format_total_number) + ".jpg in your local device"
     return
 
 def Download_the_one():
     format_image_id = raw_input("please input which image you need! (a number is required)")
     image_id = format_image_id.zfill(8)
     image_url="http://www.gunnerkrigg.com/comics/" + image_id + ".jpg";
+    Download_image(image_url, format_image_id)
+    print "The latest comic image is saved as " + format_image_id + ".jpg in your local device"
+    return
+
+def Download_image(image_url, format_image_id):
     image = urllib2.urlopen(image_url)
     output = open(format_image_id + ".jpg", "wb")
     output.write(image.read())
     output.close()
-    print "The latest comic image is saved as " + format_image_id + ".jpg in your local device"
-    return
 
 def Show_total_number():
-    print "there are " + str(format_total_number) + " comic images in this site!"
-    return
+    response=page.read()
+    pattern=re.compile(r'img class="comic_image" src="/comics/(.*).jpg">')
+    total_number= pattern.search(response).group(1)
+    format_total_number= total_number[total_number.rfind('0')+1:]
+    print "there are " + format_total_number + " comic images in this site!"
+    return total_number
 
 page=urllib2.urlopen("http://www.gunnerkrigg.com/")
-response=page.read()
-
-pattern=re.compile(r'img class="comic_image" src="/comics/(.*).jpg">')
-total_number= pattern.search(response).group(1)
-format_total_number= int(total_number)
-
 argument =raw_input("What can I do for you?(CASE SENSITIVE) A: Show me the latest comic image! B: Show me the comic image I prefer! C: How many comic images exist here?")
 Based_on_user_choice(argument)
