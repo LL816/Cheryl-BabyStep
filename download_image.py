4d7a67fe0267aa007638826685dbbaa57e1e5917
cheryl.cai <cailuruoxin@gmail.com>
2016-01-18 15:22:10 +0800

    Update download_image.py
    1. spell mistake
    2. raw_input vs input
    3. convert 000015 to 15: int() -> str()
    4. conver 15 to 000015: A.zfill(width)

--- download_image.py
+++ download_image.py
@@ -22,10 +22,8 @@ def Download_latest_one():
     return
 
 def Download_the_one():
-    format_image_id = input("please input which image you need! (a number is required)")
-    image_id = format_image_id
-    while len(image_id)<8:
-        image_id = "0" + image_id
+    format_image_id = raw_input("please input which image you need! (a number is required)")
+    image_id = format_image_id.zfill(8)
     image_url="http://www.gunnerkrigg.com/comics/" + image_id + ".jpg";
     image = urllib2.urlopen(image_url)
     output = open(format_image_id + ".jpg", "wb")
@@ -35,7 +33,7 @@ def Download_the_one():
     return
 
 def Show_total_number():
-    print "there are " + format_total_number + " comic images in this site!"
+    print "there are " + str(format_total_number) + " comic images in this site!"
     return
 
 page=urllib2.urlopen("http://www.gunnerkrigg.com/")
@@ -43,7 +41,7 @@ response=page.read()
 
 pattern=re.compile(r'img class="comic_image" src="/comics/(.*).jpg">')
 total_number= pattern.search(response).group(1)
-format_total_number= total_number[total_number.rfind('0')+1:]
+format_total_number= int(total_number)
 
-argument =input("What can I do for you?(CASE INTENSIVE) A: Show me the latest comic image! B: Show me the comic image I prefer! C: How many comic images exist here?")
+argument =raw_input("What can I do for you?(CASE SENSITIVE) A: Show me the latest comic image! B: Show me the comic image I prefer! C: How many comic images exist here?")
 Based_on_user_choice(argument)
